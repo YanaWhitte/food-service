480a08743267a4348d1f1e2635042e4fb3a59e27
YanaWhitte <shiro.kirio@gmail.com>
2021-07-10 14:31:26 +0300

    Removed README.md

--- src/components/main/Sidebar.jsx
+++ src/components/main/Sidebar.jsx
@@ -5,14 +5,14 @@ import { DashboardIcon, DiscountIcon, HomeIcon, LogoIcon, LogoutIcon, MessageIco
 const Sidebar = () => {
   return <aside>
     <div className={s.sidebar}>
-      <NavLink to="/home" activeClassName={s.active}><LogoIcon /></NavLink>
-      <NavLink to="/home" activeClassName={s.active}><HomeIcon selected /></NavLink>
-      <NavLink to="/dashboard" activeClassName={s.active}><DiscountIcon /></NavLink>
-      <NavLink to="/dashboard" activeClassName={s.active}><DashboardIcon /></NavLink>
-      <NavLink to="/dashboard" activeClassName={s.active}><MessageIcon /></NavLink>
-      <NavLink to="/setting" activeClassName={s.active}><NotificationIcon /></NavLink>
-      <NavLink to="/setting" activeClassName={s.active}><SettingIcon /></NavLink>
-      <LogoutIcon />
+      <div className={s.linkHolder}><NavLink to="/home" activeClassName={s.active}><LogoIcon /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/home" activeClassName={s.active}><HomeIcon selected /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/dashboard" activeClassName={s.active}><DiscountIcon /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/dashboard" activeClassName={s.active}><DashboardIcon /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/dashboard" activeClassName={s.active}><MessageIcon /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/setting" activeClassName={s.active}><NotificationIcon /></NavLink></div>
+      <div className={s.linkHolder}><NavLink to="/setting" activeClassName={s.active}><SettingIcon /></NavLink></div>
+      <div className={s.linkHolder}><LogoutIcon /></div>
     </div>
   </aside>
 }
